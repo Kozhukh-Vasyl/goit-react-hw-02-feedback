43daf9d89e9ebf9c535b2898f2cf426785bb3444
Kozhukh-Vasyl <v.kozhukh@gmail.com>
2023-04-25 21:30:30 +0200

    add files

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+export const FeedbackContainer = styled.div`
+  padding: 15px;
+  margin-bottom: 15px;
+`;
+export const FriendsList = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`;
+export const FriendsItem = styled.li`
+  justify-content: center;
+`;
+export const FeedbackBtn = styled.button`
+padding = 15px;
+border-radius = 2px;
+font-size: 18px;
+  font-weight: 600;
+  background-color: #bec8b3;
+  color: #000000;
+  &:hover {
+        background-color: #959c8c;
+
+`;
---
+++ src/components/Notification/Notification.styled.js
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+export const Message = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  justify-content: center;
+  text-align: center;
+`;
