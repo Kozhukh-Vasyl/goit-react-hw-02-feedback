67ffd9255a299c43f62e4ceb3d1cff4dbac112c7
Vasyl Kozhukh <113349977+Kozhukh-Vasyl@users.noreply.github.com>
2023-04-26 14:38:23 +0200

    Update App.styled.js

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -4,8 +4,8 @@ export const AppContainer = styled.div`
   padding: 16px;
   margin: 0 auto;
   width: 400px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, 
-  rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
   background-color: #e9f5db;
 `;
 
